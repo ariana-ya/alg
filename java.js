7c2c0874af5a1d1a1ce7167ad39f37f578fe4f14
ariana-ya <75086931+ariana-ya@users.noreply.github.com>
2023-11-24 16:15:40 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,62 @@
+function verify() {
+    console.log("a, b, c")
+    let a = parseInt(elementA.value);
+    let b = parseInt(elementB.value);
+    let c = parseInt(elementC.value);
+    console.log(a, b, c)
+
+     if (a > b && b > c) {
+        result = "Первое выполняется"
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = true;
+    } else if (a < b && b < c) {
+        result = "Второе выполняется"
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = true;
+    } else {
+        result = "Никакое не выполняется"
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = true;
+    }
+}
+
+function send() {
+    if (check) {
+        let textCondition = document.getElementsByTagName('p')[0].innerText
+        document.getElementsByName('formulation')[0].value = textCondition;
+        document.getElementsByName('result')[0].value = result;
+        document.getElementById("UserEnter").submit();
+    } else {
+        alert("Есть недостатки. Повторите ввод")
+    }
+}
+
+function verify_send() {
+    verify();
+    send();
+}
+
+let messageText = document.getElementById("result").innerText
+console.log(messageText)
+let result;
+let check = false;
+
+const elementA = document.getElementById("a");
+elementA.addEventListener('input', verify);
+
+const elementB = document.getElementById("b");
+elementB.addEventListener('keyup', verify);
+
+const elementC = document.getElementById("c");
+elementC.addEventListener('keyup', verify);
+
+const elementVerify = document.getElementById("verify");
+elementVerify.addEventListener('click', verify);
+
+const elementSend = document.getElementById("send");
+elementSend.addEventListener('click', send)
+
+
